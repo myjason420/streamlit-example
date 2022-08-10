3199bbc7cfdaa6b202d1ddc6af2ce0c11557321c
myjason420 <110984740+myjason420@users.noreply.github.com>
2022-08-10 18:52:01 +0530

    Update stcomp.py

--- stcomp.py
+++ stcomp.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://www.reuters.com/world/india/")
+components.iframe("https://api.digitallocker.gov.in/public/oauth2/1/authorize?response_type=code&client_id=1400793F&redirect_uri=https://www.godigit.com/callback&state=codex1234",width=250, height=250, scrolling=True)
